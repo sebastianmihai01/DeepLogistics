fee52357340cfa7ff2062c5f05955ab1ceea34b4
Sebi <mihaisebastian01@gmail.com>
2021-06-15 18:47:00 +0200

    finished pol lin reg

--- LinearRegression/LinearRegression.py
+++ LinearRegression/LinearRegression.py
@@ -1,9 +1,12 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
-x = np.array([5, 15, 25, 35, 45, 55]).reshape((-1, 1))
-y = np.array([5, 20, 14, 32, 22, 38])
-model = LinearRegression().fit(x, y)
-r_sq = model.score(x, y)
-print('coefficient of determination:', r_sq)
-print('intercept:', model.intercept_)
-print('slope:', model.coef_)
+
+#For explanation see documentation of "PolynomialLinearRegression.py"
+def lin_reg(x,y):
+    x = np.array(x).reshape((-1, 1))
+    y = np.array(y)
+    model = LinearRegression().fit(x, y)
+    r2 = model.score(x, y)
+    print('R^2 is :', r2)
+    print('interception (b0) with y axis:', model.intercept_)
+    print('slope of the function', model.coef_)
--- LinearRegression/PolynomialLinearRegression.py
+++ LinearRegression/PolynomialLinearRegression.py
@@ -1,29 +1,71 @@
-# Step 1: Import packages
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 
-# Step 2a: Provide data
-x = [[0, 1], [5, 1], [15, 2], [25, 5], [35, 11], [45, 15], [55, 34], [60, 35]]
-y = [4, 5, 20, 14, 32, 22, 38, 43]
-x, y = np.array(x), np.array(y)
+#ValDegree = polynomial degree
+#bias = boolean true/false for bias approximations
 
-# Step 2b: Transform input data
-x_ = PolynomialFeatures(degree=2, include_bias=False).fit_transform(x)
+def populateBy_observation_points(list_x,list_y):
+    nr_obs_points = np.size(list_x) #nr of observation points
 
-# Step 3: Create a model and fit it
-model = LinearRegression().fit(x_, y)
+    #lines for x and y (means)
 
-# Step 4: Get results
-r_sq = model.score(x_, y)
-intercept, coefficients = model.intercept_, model.coef_
+    meanX = np.mean(list_x)
+    meanY = np.mean(list_y)
 
-# Step 5: Predict
-y_pred = model.predict(x_)
+    #difference between plot and obs point = deviation
+    #calculation of the deviation is the following:
 
-print('coefficient of determination:', r_sq)
-print('intercept:', intercept)
-intercept: 0.8430556452395734
-print('coefficients:', coefficients, sep='\n')
-[ 2.44828275  0.16160353 -0.15259677  0.47928683 -0.4641851 ]
-print('predicted response:', y_pred, sep='\n')
+    SS_xy = np.sum(list_y * list_x) - nr_obs_points * meanY * meanX
+    SS_xx = np.sum(list_x * list_x) - nr_obs_points * meanX * meanX
+
+    #slope:
+    slope = SS_xx
+
+    #intersection with y axis:
+    intersection = meanY - slope*meanX
+
+
+def pol_reg(x,y,valDegree,bias):
+    try:
+        #numpy arrays, specific for lin regression
+        x = np.array(x)
+        y = np.array(y)
+        
+        formattedX = PolynomialFeatures(degree=valDegree, include_bias=bias).fit_transform(x)
+        model = LinearRegression().fit(formattedX, y)
+        score = model.score(formattedX, y)
+        intercept, coefficients = model.intercept_, model.coef_
+        predictedY = model.predict(formattedX)
+
+        print('R^2: coeff. of determination:', score)
+        print('b0: interception with the y axis:', intercept)
+        print('coeff:', coefficients, sep='\n')
+        print('prediction:', predictedY, sep='\n')
+    except Exception as exc:
+        print("Errors in any of the arrays 'x' or 'y'")
+
+
+def plot(list_x, list_y, b):
+
+    #plotting the points
+    plt.scatter(list_x, list_y, color="m",marker="X", s=10)
+    y_pred = b[0] + b[1] * list_x
+
+    #plotting the line
+    plt.plot(list_x, y_pred, color="b")
+
+    plt.xlabel('oX')
+    plt.ylabel('oY')
+    plt.show()
+
+    coef = populateBy_observation_points(list_x, list_y)
+    b_0 = coef[0]
+    b_1 = coef[1]
+
+    plot(list_x,list_y,coef)
+
+    #perhaps not working
+    #perhaps2 only for polynomials of degree 2
+    print("linear regression function: f(x) = ",b_0 ," + ", b_1, "x")
